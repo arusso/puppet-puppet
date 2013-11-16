a8961f76df889a77c2103cdbc6051346fbccdbbe
Aaron Russo <aaron.n.russo@gmail.com>
2013-11-16 08:46:22 -0800

    start of refactor. probably going to ditch

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,51 @@
 #
 # === Parameters:
 #
-# [*dns_alt_names*]
+# [*bucketdir*]
+#   (optional) path where bucket files are stored.
 #
-# (Master only) DNS Alternative Names to use when generating the Puppet master
-# ssl cert
+#  Default: $vardir/bucket
+#
+# [*ca*]
+#   (optional) true/false - sets ca = $ca in the [master] section.
+#
+# [*ca_name*]
+#   (optional) the name of the Puppet CA server
+#
+# [*ca_port*]
+#   (optional) the port the Puppet CA will be listening on
+#
+#   Default: $masterport
+#
+# [*ca_server*]
+#   (optional) if set, points the agent to a central ca_server
+#
+#  Default: $server
+#
+# [*ca_ttl*]
+#   (optional) the TTL for new certificates.
+#
+#   Default: 5y
+#
+# [*cacert*]
+#   (optional) the location of the ca certificate.
+#
+#   Default: $cadir/ca_crt.pem
+#
+# [*cacrl*]
+#   (optional) the ca revocation list for the CA. Used if present, otherwise
+#   ignored.
+#
+#   Default: $cadir/ca_crl.pem
+#
+# [*cadir*]
+#  (optional) the root directory for the certificate authority
+#
+#  Default: $ssldir/ca
+#
+# [*dns_alt_names*]
+#   (Master only) DNS Alternative Names to use when generating the Puppet
+#   master ssl cert
 #
 # [*manifest*]
 #
@@ -50,22 +91,99 @@
 #
 # (Master only) specifies the location of the tagmap file
 #
+# Awesomeness:
+#
+# s/\$\([a-z_]\+\)\(\s\+=hiera('puppet::\1',\ \)'UNSET'\(),\)/
+#                                                    $\1\2$puppet::param::\1\3/
 class puppet (
-  $dns_alt_names = undef,
-  $manifest = undef,
-  $master = undef,
-  $modulepath = undef,
-  $pluginsync = undef,
-  $reports = undef,
-  $reportfrom = undef,
-  $sendmail = undef,
-  $server = undef,
-  $service = undef,
-  $tagmap = undef
-) {
-  # Agent installer
-  include puppet::agent
-  Class['puppet'] -> Class['puppet::agent']
+  $agent                  =hiera('puppet::agent', 'UNSET'),
+  $allow_duplicate_certs  =hiera('puppet::allow_duplicate_certs', 'UNSET'),
+  $archive_file_server    =hiera('puppet::archive_file_server', 'UNSET'),
+  $archive_files          =hiera('puppet::archive_files', 'UNSET'),
+  $bindaddress            =hiera('puppet::bindaddress', 'UNSET'),
+  $bucketdir              =hiera('puppet::bucketdir', 'UNSET'),
+  $ca                     =hiera('puppet::ca', 'UNSET'),
+  $ca_name                =hiera('puppet::ca_name', 'UNSET'),
+  $ca_port                =hiera('puppet::ca_port', 'UNSET'),
+  $ca_server              =hiera('puppet::ca_server', 'UNSET'),
+  $ca_ttl                 =hiera('puppet::ca_ttl', 'UNSET'),
+  $cacert                 =hiera('puppet::cacert', 'UNSET'),
+  $cacrl                  =hiera('puppet::cacrl', 'UNSET'),
+  $cadir                  =hiera('puppet::cadir', 'UNSET'),
+  $cakey                  =hiera('puppet::cakey', 'UNSET'),
+  $capass                 =hiera('puppet::capass', 'UNSET'),
+  $caprivatedir           =hiera('puppet::caprivatedir', 'UNSET'),
+  $capub                  =hiera('puppet::capub', 'UNSET'),
+  $cert_inventory         =hiera('puppet::cert_inventory', 'UNSET'),
+  $certdir                =hiera('puppet::certdir', 'UNSET'),
+  $certificate_revocation =hiera('puppet::certificate_revocation', 'UNSET'),
+  $certname               =hiera('puppet::certname', 'UNSET'),
+  $classfile              =hiera('puppet::classfile',
+                                  $puppet::param::classfile),
+  $confdir                =hiera('puppet::confdir', 'UNSET'),
+  $config                 =hiera('puppet::config', $puppet::param::config),
+  $dns_alt_names          =hiera('puppet::dns_alt_names', 'UNSET'),
+  $environment            =hiera('puppet::environment', 'UNSET'),
+  $localconfig            =hiera('puppet::localconfig',
+                                  $puppet::param::localconfig),
+  $logdir                 =hiera('puppet::logdir', $puppet::param::logdir),
+  $master                 =hiera('puppet::master', 'UNSET'),
+  $manifest               =hiera('puppet::manifest', 'UNSET'),
+  $manage_service         =hiera('puppet::manage_service', 'UNSET'),
+  $module_repository      =hiera('puppet::module_respository', 'UNSET'),
+  $module_working_dir     =hiera('puppet::module_working_dir', 'UNSET'),
+  $modulepath             =hiera('puppet::modulepath', 'UNSET'),
+  $postrun_command        =hiera('puppet::postrun_command', 'UNSET'),
+  $pluginsync             =hiera('puppet::pluginsync', 'UNSET'),
+  $prerun_command         =hiera('puppet::prerun_command', 'UNSET'),
+  $report                 =hiera('puppet::report', $puppet::param::report),
+  $report_server          =hiera('puppet::report_server', 'UNSET'),
+  $reportfrom             =hiera('puppet::reportfrom', 'UNSET'),
+  $reports                =hiera('puppet::reports', 'UNSET'),
+  $reporturl              =hiera('puppet::reporturl', 'UNSET'),
+  $rundir                 =hiera('puppet::rundir', $puppet::param::rundir),
+  $sendmail               =hiera('puppet::sendmail', 'UNSET'),
+  $ssldir                 =hiera('puppet::ssldir', $puppet::param::ssldir),
+  $server                 =hiera('puppet::server', 'UNSET'),
+  $tagmap                 =hiera('puppet::tagmap', 'UNSET'),
+) inherits puppet::param {
+
+  Ini_setting{
+    ensure            => present,
+    path              => $config,
+    section           => 'main',
+    key_val_separator => ' = ',
+  }
+
+  $req_main_settings = {
+    'logdir' => { setting => 'logdir',
+                  value   => $logdir, },
+
+    'rundir' => { setting => 'rundir',
+                  value   => $rundir, },
+
+    'ssldir' => { setting => 'ssldir',
+                  value   => $ssldir, },
+  }
+  create_resources( 'ini_setting', $req_main_settings )
+
+
+  if $agent {
+    class { 'puppet::agent':
+      ca_port     => $ca_port,
+      ca_server   => $ca_server,
+      classfile   => $classfile,
+      report      => $report,
+      localconfig => $localconfig,
+    }
+  }
+
+  if $master {
+    class { 'puppet::master':
+      dns_alt_names => $dns_alt_names,
+
+    }
+  }
 
   # Master installer
   $master_r = $master ? {
